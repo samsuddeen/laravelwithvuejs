b2b9ff605a7de4aa8477e42425edbe299e7b4a67
samsuddeen <samsuddeenaalam7@gmail.com>
2025-06-16 23:53:32 +0545

    1st day

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+
+import ExampleComponent from './components/ExampleComponent.vue';
+
+createApp(ExampleComponent).mount('#app');
+
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>welcome to the vuejs {{ name }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ExampleComponent",
+    data(){
+        return{
+            name: 'king'
+        }
+    }
+
+}
+
+</script>
+
+<style scoped>
+h1{
+    color:red;
+}
+</style>
